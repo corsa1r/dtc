0ad3f48c9fd8a290f43fdc2946af1a224c0f657b
Vladimir Enchev <vladimir.corsair@gmail.com>
2021-03-19 14:09:56 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,123 @@
+let degrees = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (input.temperature()))
+})
+function roll () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    control.waitMicros(0.1)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # . # #
+        # # # # #
+        # # # # #
+        `)
+    control.waitMicros(0.1)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    control.waitMicros(0.1)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    control.waitMicros(0.1)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    control.waitMicros(0.1)
+    showResult(randint(1, 6))
+}
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+function showNorth () {
+    if (degrees < 45) {
+        basic.showArrow(ArrowNames.North)
+    } else if (degrees < 135) {
+        basic.showArrow(ArrowNames.East)
+    } else if (degrees < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else if (degrees < 315) {
+        basic.showArrow(ArrowNames.West)
+    } else {
+        basic.showArrow(ArrowNames.North)
+    }
+}
+function showResult (num: number) {
+    if (num == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (num == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (num == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (num == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (num == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+}
+input.onGesture(Gesture.Shake, function () {
+    roll()
+})
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (input.buttonIsPressed(Button.B)) {
+        showNorth()
+    }
+})
